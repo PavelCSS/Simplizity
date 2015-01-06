5461b72fb0901d55494c33fd769bfa2c380f2101
PavelCSS <yashin.ziby@gmail.com>
2015-01-06 11:24:46 +0200

    Update

--- plugins/org.apache.cordova.vibration/src/wp/Vibration.cs
+++ plugins/org.apache.cordova.vibration/src/wp/Vibration.cs
@@ -1,4 +1,4 @@
-﻿﻿/*  
+﻿/*  
 	Licensed under the Apache License, Version 2.0 (the "License");
 	you may not use this file except in compliance with the License.
 	You may obtain a copy of the License at
@@ -27,11 +27,11 @@ namespace WPCordovaClassLib.Cordova.Commands
 {
     public class Vibration : BaseCommand
     {
+        private static readonly int DEFAULT_DURATION = 200;
 
         public void vibrate(string vibrateDuration)
         {
-
-            int msecs = 200; // set default
+            int msecs = DEFAULT_DURATION; // set default
 
             try
             {
@@ -48,9 +48,29 @@ namespace WPCordovaClassLib.Cordova.Commands
 
             }
 
-            VibrateController.Default.Start(TimeSpan.FromMilliseconds(msecs));
+            vibrateMs(msecs);
 
             // TODO: may need to add listener to trigger DispatchCommandResult when the vibration ends...
+            DispatchCommandResult();
+        }
+
+        private static void vibrateMs(int msecs)
+        {
+            VibrateController.Default.Start(TimeSpan.FromMilliseconds(msecs));
+        }
+
+        public void vibrateWithPattern(string options)
+        {
+            // falling back to vibrate
+            vibrateMs(DEFAULT_DURATION);
+
+            // TODO: may need to add listener to trigger DispatchCommandResult when the vibration ends...
+            DispatchCommandResult();
+        }
+
+        public void cancelVibration(string options)
+        {
+            VibrateController.Default.Stop();
             DispatchCommandResult();
         }
     }
